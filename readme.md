88b496495335475208c81ee4f9766710dca2d3ce
Lenadubovets <lenadubovets@hotmail.com>
2022-12-02 10:14:19 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+## **The project**
+The goal of the project was to create a web application that:
+- takes the user's input (name, age, and city)
+- shows the user's age and their Chinese Zodiac, together with an image and a brief description of the Zodiac, as well as the current weather in the user's city
+
+## Tools
+
+To reach our goal, we used the following tools:
+- HTML
+- CSS and Bootstrap
+- JavaScript
+- Weather API (shorturl.at/yBFMW)
+- session storage
+
+## Developers
+- Mariandrea Villalobos
+- Olena Dubovets
+- Zuzanna Zahorska
---
+++ weather.js
@@ -0,0 +1,33 @@
+// fetch van jsonplaceholder API;
+// Update
+let mySession = JSON.parse(sessionStorage.getItem("dataObject"));
+console.log("test" + mySession);
+
+let weather = document.getElementById("weather");
+let url =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+  mySession.inputcity +
+  "?unitGroup=metric&include=current&key=JN5WK3U2N95ZKUL2QSK3D8R2P&contentType=json";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    if (data.currentConditions.conditions == "Overcast") {
+      document.getElementById("myImg").src = "Images/overcast.gif";
+    } else if (data.currentConditions.conditions == "Partially cloudy") {
+      document.getElementById("myImg").src = "Images/overcast.gif";
+    } else if ((data.currentConditions.conditions == "Rain", "Overcast")) {
+      document.getElementById("myImg").src = "Images/rain.gif";
+    } else if (data.currentConditions.conditions == "Clear") {
+      document.getElementById("myImg").src = "Images/4lQ5.gif";
+    }
+    weather.innerHTML = `
+  <p>Location: ${data.resolvedAddress}</p>
+  <h4>Temperature: ${data.currentConditions.temp}°C</h4>
+  <p>Current conditions: ${data.currentConditions.conditions}</p>`;
+  })
+  .catch((err) => {
+    alert("Wrong city name");
+    console.log(err);
+  });
